6c93e5d179676a6fa6af9db6e09f70149939bdc1
blurryfacebeat <blurryfacebeat@gmail.com>
2024-01-10 22:02:42 +0700

    feat: Add messages listener

--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -11,9 +11,22 @@ export class TelegramService {
   constructor(@Inject(TELEGRAM_MODULE_OPTIONS) options: TelegramOptions) {
     this.options = options;
     this.bot = new TelegramBot(options.token, { polling: true });
+
+    this.messagesListener();
   }
 
-  async sendMessage(message: string, chatId: string = this.options.chatId) {
+  async sendMessage(
+    message: string,
+    chatId: string | number = this.options.chatId,
+  ): Promise<void> {
     await this.bot.sendMessage(chatId, message);
+  }
+
+  async messagesListener(): Promise<void> {
+    this.bot.on('message', (message) => {
+      console.log(123, message);
+
+      this.sendMessage(JSON.stringify(message), message.chat.id);
+    });
   }
 }
